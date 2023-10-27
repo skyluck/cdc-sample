c0800cd3c7d76858c51a4283307647f494618a23
Kevin.Bai <kevin.bai@webcomm.com.tw>
2023-10-27 17:10:29 +0800

    stub can publish to nexus

--- provider-gradle/src/test/java/cdc/BaseTestClass.java
+++ provider-gradle/src/test/java/cdc/BaseTestClass.java
@@ -4,12 +4,10 @@ import com.cdc.providergradle.ProviderGradleApplication;
 import com.cdc.providergradle.controllers.EvenOddController;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
 
